70d4f2422601eb2ed3c213ca6ba7b33cc8c2eb53
kaur0762 <kaur0762@algonquinlive.com>
2022-03-05 22:23:56 -0500

    Add Mongoose db connection to Express app

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Don't forget to use NPM to install Express and Mongoose.
+'use strict'
+
+const mongoose = require('mongoose')
+mongoose
+    .connect('mongodb://localhost:27017/mad9124', {
+    useNewUrlParser: true
+    })
+    .then(() => console.log('Connected to MongoDB ...'))
+    .catch(err => {
+    console.error('Problem connecting to MongoDB ...', err.message)
+    process.exit(1)
+    })
+
+const morgan = require('morgan')
+const express = require('express')
+const app = express()
+
+app.use(morgan('tiny'))
+app.use(express.json())
+
+const port = process.env.PORT || 3030
+app.listen(port, () => console.log(`HTTP server listening on port ${port} ...`))
