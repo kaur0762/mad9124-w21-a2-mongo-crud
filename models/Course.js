0e3d32e403e1975c9f1c59cebc0fec9d5eb3684f
kaur0762 <kaur0762@algonquinlive.com>
2022-03-05 22:28:38 -0500

    Add the Mongoose model for Course

---
+++ models/Course.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    code: String,
+    title: String,
+    description: String,
+    url: String
+})
+const Model = mongoose.model('Course', schema)
+
+module.exports = Model
