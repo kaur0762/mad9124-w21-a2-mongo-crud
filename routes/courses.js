27fb50fdfbd9d85513f13c4048bcabdc2ae7be99
kaur0762 <kaur0762@algonquinlive.com>
2022-03-05 22:47:21 -0500

    Add the GET by id for courses route logic

--- routes/courses.js
+++ routes/courses.js
@@ -17,13 +17,33 @@ router.post('/', async (req, res) => {
     res.status(201).send({data: newCourse})
 })
 
-router.get('/:id', async (req, res) => {})
+router.get('/:id', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id)
+        if (!course) throw new Error('Resource not found')
+        res.send({data: course})
+    } catch (err) {
+        sendResourceNotFound(req, res)
+    }
+})
 
 router.patch('/:id', async (req, res) => {})
 
 router.put('/:id', async (req, res) => {})
 
 router.delete('/:id', async (req, res) => {})
+
+function sendResourceNotFound(req, res) {
+    res.status(404).send({
+        errors: [
+        {
+            status: '404',
+            title: 'Resource does not exist',
+            description: `We could not find a course with id: ${req.params.id}`
+        }
+        ]
+    })
+}
 
 /**
  * Format the response data object according to JSON:API v1.0
