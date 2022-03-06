4281de5743f212a40801f971ec1ebe69ec15174d
kaur0762 <kaur0762@algonquinlive.com>
2022-03-05 22:31:05 -0500

    Add courses router module

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express()
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use('/api/courses', require('./routes/courses'))
 
 const port = process.env.PORT || 3030
 app.listen(port, () => console.log(`HTTP server listening on port ${port} ...`))
---
+++ routes/courses.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async (req, res) => {})
+
+router.post('/', async (req, res) => {})
+
+router.get('/:id', async (req, res) => {})
+
+router.patch('/:id', async (req, res) => {})
+
+router.put('/:id', async (req, res) => {})
+
+router.delete('/:id', async (req, res) => {})
+
+/**
+ * Format the response data object according to JSON:API v1.0
+ * @param {string} type The resource collection name, e.g. 'cars'
+ * @param {Object} resource An instance object from that collection
+ * @returns
+ */
+function formatResponseData(type, resource) {
+    const {id, ...attributes} = resource
+    return {type, id, attributes}
+}
+
+module.exports = router
